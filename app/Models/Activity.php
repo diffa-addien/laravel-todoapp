b3368dbd03b02aa62e815a9bac2369eec04d6b7d
Diffaddien <diffaddien@gmail.com>
2024-12-10 16:44:35 +0700

    change(routing): fix routing

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Activity extends Model
 {
     use HasFactory;
+    // Tambahkan atribut yang diizinkan untuk mass assignment
+    protected $fillable = [
+        'title',
+        'user_id',
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\ItemController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/login', function () {
+    return response()->json(['message' => 'Unauthenticated'], 401);
+})->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/activities', [ActivityController::class, 'store']);
